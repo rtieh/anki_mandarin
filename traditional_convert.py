6968ddba3770211952356f0885e966c35b7cf192
dehao tieh <rowantieh@gmail.com>
2026-03-13 02:28:43 -0700

    Enhance comments in traditional_convert.py
    Added comments for clarity and instruction on usage.

--- traditional_convert.py
+++ traditional_convert.py
@@ -1,7 +1,11 @@
+#this is for if, after formatting your words correctly into the 'word', 'word', etc. format,
+#you have simplified words and need to convert everything into traditional characters
+
 from opencc import OpenCC
 
-cc = OpenCC('s2t')  # simplified → traditional
+cc = OpenCC('s2t')  
 
+#input 'word', 'word', etc. here
 words = [
 '養老金', ' 透頂', '毛茸茸', '遊子', '駱駝', '俯臥', '引體', '建模', '反鸟托邦', '羞愧', '内鬼', '浴室', '写爱无緣', '謠言', '辛虧', '探望', '挥舞', '歡呼', '謙虛', '致意', '壓倦', '單調', '承讓', '富裕', '耸肩', '不以為然', '羞愧', '怠惰', '令人髮指', '糾正', '雞皮疙瘩', '留意', '得意', '自大', '隱晦', '走神', '佔有', '後頸', '愉悅', '含糊', '探究', '肩胛骨', '羞恥', '塌下', '僻字', '磨蹭', '咕噥', '摩娑', '珊瑚', '尖', '评估', '摊派', '繁殖', '抹面', '缺陷', '溫馨', '胡话', '心坎', '洋人', '點燃', '正經', '关节炎', '缺乏', '蒼蠅', '准确', '富裕', '岛屿', '注册', '专利', '倒是', '意味', '歌唱', '重蹈覆轍 ', '王八蛋', '洗漱', '榴蓮', '念想', '健忘', '迷戀', '堵住', '罪過', '褪色', '情愫', '知己', '瓢潑', '房檐', '身影', '慶幸', '水道', '抑制', '隨波逐流', '放蕩', '慈祥', '背影', '便利店', '傻瓜', '璀璨'
 ]
@@ -11,3 +15,4 @@ converted = [cc.convert(w.strip()) for w in words]
 formatted = ", ".join(f"'{w}'" for w in converted)
 
 print(formatted)
+#copy this result and paste it into the wordlist for anki_algorithm1
